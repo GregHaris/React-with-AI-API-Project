e10de9771768f877ed3d30f8e3c75db974fe22d1
Grëg Häris <d.gregharis@gmail.com>
2024-10-21 12:12:28 +0100

    feat: add heading component

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,11 @@
+import Headings from './component/Headings';
 
+function App() {
+  return (
+    <div>
+      <Headings heading="Hi, Welcome" children="How can we help you today?" />
+    </div>
+  );
+}
+
+export default App;
