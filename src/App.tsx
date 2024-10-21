bb7d2b5df512a18e5147fb1de41eb2650b12ef68
Grëg Häris <d.gregharis@gmail.com>
2024-10-21 11:23:29 +0100

    clear up files

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,1 @@
-import { useState } from 'react';
-import Alert from './components/Alert';
-import Button from './components/Button';
 
-function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
-  return (
-    <div>
-      {alertVisible && <Alert onClose={() => setAlertVisibility(false)}> Congratulations 🎉 </Alert>}
-      <Button color="primary" onClick={() => setAlertVisibility(true)}>
-        Click me
-      </Button>
-    </div>
-  );
-}
-
-export default App;
