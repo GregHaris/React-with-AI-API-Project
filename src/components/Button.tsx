dc3bcd6ec86e229807eb88b1f5dc795c8a672835
Grëg Häris <d.gregharis@gmail.com>
2024-10-22 23:40:24 +0100

    feat: add optional disable button prop
    A prop to disable the button when needed

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,8 @@
 interface Props {
   textContent: string;
   handleClick: () => void;
+  // optional logic to disable the button
+  disabled?: boolean 
 }
 
 const Button = ({ textContent, handleClick }: Props) => {
